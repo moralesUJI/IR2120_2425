ca1560664a1fddb402a11475663858cface5f95a
Antonio Morales <moralesUJI@users.noreply.github.com>
2024-09-26 17:05:54 +0200

    Update install_rvc3.sh

--- scripts/install_rvc3.sh
+++ scripts/install_rvc3.sh
@@ -1,23 +1,25 @@
 # Run this script to install mamba envoriment manager and RVC3 on your system
-# It will create a folder rvc3_ws in the current directory and will modify .bashrc 
-# to allow the execution of mamba commands
-,
-!/bin/bash
+# It will create a folder rvc3_ws in the user root directory and will modify .bashrc 
+# to allow the execution of mamba command
+# Remember to change the execution permissions
+#   chmod u+x installrvc3.sh
+# the execute with
+#   . install_rvc3.sh
 
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh
+cd ~ 
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+
 bash Miniforge3-$(uname)-$(uname -m).sh
 
 source ~/.bashrc
 
-mamba create -n rvc3 python=3.10
+mamba create -n rvc3 python=3.10pi
 mamba activate rvc3
 pip install rvc3python
-pip install ipympl 
+pip install ipympl
 
 mkdir rvc3_ws
 cd rvc3_ws
 git clone https://github.com/petercorke/RVC3-python.git
 
 echo "Run 'rvctool -n -a -c' to start the RVC3 tool" 
-
-
